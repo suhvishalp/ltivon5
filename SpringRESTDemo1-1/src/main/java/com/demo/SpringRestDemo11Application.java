8780fed556316e7d8fd09801dcafb592a8328368
Vishal <vishal@Vishals-MacBook-Pro.local>
2021-12-09 16:39:25 +0530

    -

---
+++ SpringRESTDemo1-1/src/main/java/com/demo/SpringRestDemo11Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDemo11Application.class, args);
+	}
+
+}
